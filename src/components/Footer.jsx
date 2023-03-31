c906804a617eaa1bff1c5afe61a883984c52ac90
qamarali205 <qamarali205@gmail.com>
2023-03-31 23:43:26 +0530

    dashboard boiler plate

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2024 All rights reserved by The Web Warriors
+    </p>
+  </div>
+);
+
+export default Footer;
