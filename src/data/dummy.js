a816dc565a3c390f526c1184de15aef28214344c
qamarali205 <qamarali205@gmail.com>
2023-04-01 17:21:54 +0530

    app.js dummy data

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -512,10 +512,10 @@ export const links = [
         name: 'editor',
         icon: <FiEdit />,
       },
-      {
-        name: 'color-picker',
-        icon: <BiColorFill />,
-      },
+      // {
+      //   name: 'color-picker',
+      //   icon: <BiColorFill />,
+      // },
     ],
   },
   {
