b394c83c987328cb01e4022e0f13bb2413899cb7
qamarali205 <qamarali205@gmail.com>
2023-04-01 17:57:45 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
-import Login from './pages/login/Login.jsx'
+// import Login from './pages/login/Login.jsx'
 import './App.css';
 
 import { useStateContext } from './contexts/ContextProvider';
@@ -73,7 +73,7 @@ const App = () => {
                 <Route path="/orders" element={<Orders />} />
                 <Route path="/employees" element={<Employees />} />
                 <Route path="/customers" element={<Customers />} />
-                <Route path="/login" element={<Login />} />
+                {/* <Route path="/login" element={<Login />} /> */}
 
                 {/* apps  */}
                 <Route path="/kanban" element={<Kanban />} />
